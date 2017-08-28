e8ab9a625879f9442adb5bba865d2b9219b025b4
rahulnema <rahulnema@rahulnema-Aspire-E5-571>
2017-08-28 20:16:50 +0530

    new commit

---
+++ MyProgram.cpp
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#include <iostream>
+
+using namespace std;
+
+main(int argc, char** argv){
+    
+    
+    int x ;
+    int y ;
+    
+    cin>>x;
+    cin>>y;
+    int z = x+y;
+    cout<<"value is " <<z;
+}
